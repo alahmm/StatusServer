c8ef077260cf59af9c9ea277c4046e5a80889838
Meriem Al Ahmadi Hammou <Meriem.Al.Ahmadi.Hammou@msg.group>
2024-11-05 08:55:32 +0100

    Add host property to the component

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -6,7 +6,10 @@ import {Component, input, ViewEncapsulation} from '@angular/core';
   imports: [],
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
-  encapsulation: ViewEncapsulation.None//to activate the use of css design for inputs and textarea
+  encapsulation: ViewEncapsulation.None,//to activate the use of css design for inputs and textarea
+  host: {
+    class: 'control'
+  }
 })
 export class ControlComponent {
   label = input.required<string>();
