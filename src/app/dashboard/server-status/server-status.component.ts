6d729d1e6a4179ae125c4aaf7afce01b7f69f0ea
Meriem Al Ahmadi Hammou <Meriem.Al.Ahmadi.Hammou@msg.group>
2024-11-05 19:34:58 +0100

    Use ngOnInit and ngOnDestroy

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,10 +7,14 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
+export class ServerStatusComponent implements OnInit, OnDestroy {
   currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
-
+  private interval?: ReturnType<typeof setInterval>;
   constructor() {
+
+  }
+
+  ngOnInit() {
     setInterval(() => {
       const rnd = Math.random();// 0-0.9999
       if(rnd < 0.5) {
@@ -21,5 +25,13 @@ export class ServerStatusComponent {
         this.currentStatus = 'unknown';
       }
     }, 5000)
+  }
+
+  ngAfterViewInit() {
+    console.log('AFTER VIEW INIT')
+  }
+
+  ngOnDestroy() {
+    clearTimeout(this.interval);
   }
 }
