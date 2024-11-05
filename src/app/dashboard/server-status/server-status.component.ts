d7b77e209b3c5bffb9ec07b56d6867a36b6c258c
Meriem Al Ahmadi Hammou <Meriem.Al.Ahmadi.Hammou@msg.group>
2024-11-05 19:56:43 +0100

    Use Template Variables

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, DestroyRef, inject, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,31 +7,31 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent implements OnInit, OnDestroy {
+export class ServerStatusComponent implements OnInit {
   currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
-  private interval?: ReturnType<typeof setInterval>;
+  private destroyRef = inject(DestroyRef);
   constructor() {
 
   }
 
   ngOnInit() {
-    setInterval(() => {
+    const interval = setInterval(() => {
       const rnd = Math.random();// 0-0.9999
-      if(rnd < 0.5) {
+      if (rnd < 0.5) {
         this.currentStatus = 'online';
-      } else if(rnd < 0.9) {
+      } else if (rnd < 0.9) {
         this.currentStatus = 'offline';
       } else {
         this.currentStatus = 'unknown';
       }
-    }, 5000)
+    }, 5000);
+
+    this.destroyRef.onDestroy(() => {
+      clearInterval(interval);
+    });
   }
 
   ngAfterViewInit() {
     console.log('AFTER VIEW INIT')
-  }
-
-  ngOnDestroy() {
-    clearTimeout(this.interval);
   }
 }
